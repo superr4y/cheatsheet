541f3590d36a48460f5bcbd904ba026e2bd118e8
superr4y <superr4y@gmail.com>
2016-08-15 00:55:21 +0200

    added dunst and improved i3 display mode

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-ln -s $(pwd)/i3 ~/.i3
-ln -s $(pwd)/tmux/tmux.conf ~/.tmux.conf
-ln -s $(pwd)/urxvt/Xresources ~/.Xresources
-ln -s $(pwd)/vim/vimrc.txt ~/.vimrc
+ln -s "$(pwd)"/i3 ~/.i3
+ln -s "$(pwd)"/tmux/tmux.conf ~/.tmux.conf
+ln -s "$(pwd)"/urxvt/Xresources ~/.Xresources
+ln -s "$(pwd)"/vim/vimrc.txt ~/.vimrc
 mkdir ~/.urxvt
 mkdir ~/.urxvt/ext
-ln -s $(pwd)/urxvt/urxvt_clipboard ~/.urxvt/ext/clipboard
-ln -s $(pwd)/zshrc ~/.zshrc
+ln -s "$(pwd)"/urxvt/urxvt_clipboard ~/.urxvt/ext/clipboard
+ln -s "$(pwd)"/zshrc ~/.zshrc
+mkdir ~/.config/dunst
+ln -s "$(pwd)"/dunstrc ~/.config/dunst/dunstrc
